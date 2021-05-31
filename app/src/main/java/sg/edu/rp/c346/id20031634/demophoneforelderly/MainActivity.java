8bf21430aa632da8c8ac968757806c1051094994
CHIW JIA LONG <20031634@myrp.edu.sg>
2021-05-31 18:45:31 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20031634/demophoneforelderly/MainActivity.java
@@ -0,0 +1,35 @@
+package sg.edu.rp.c346.id20031634.demophoneforelderly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn, btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.buttonCallSon);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" +92240336));
+                startActivity(intentCall);
+            }
+        });
+        btn1 =findViewById(R.id.buttonCallDaughter);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" +89675543));
+                startActivity(intentCall);
+            }
+        });
+    }
+}
